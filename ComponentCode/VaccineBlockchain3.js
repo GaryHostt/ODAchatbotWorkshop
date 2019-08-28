9d72f3ba730c0f85f45f1c965e08537a7a2221ea
Alex MacDonald <34657980+GaryHostt@users.noreply.github.com>
2019-08-28 16:19:13 -0700

    Update VaccineBlockchain3.js

--- ComponentCode/VaccineBlockchain3.js
+++ ComponentCode/VaccineBlockchain3.js
@@ -20,24 +20,23 @@ module.exports = {
 
 // conversation.logger().info("Input parameter values: vaccineType: " + vaccineType + ", NameOfHospital: "+NameOfHospital+", RecalledStatus: "+RecalledStatus);
 
-var args = "["+vaccineType+","+QuantityOfVaccine+","+NameOfHospital+","+RecalledStatus+"]"
-        
+// var args = "["+vaccineType+","+QuantityOfVaccine+","+NameOfHospital+","+RecalledStatus+"]"
+
       request('https://28aff79ced104f77b12adc70669d8efa.blockchain.ocp.oraclecloud.com/restproxy1',
-        {method: 'POST',
+        {
+        method: 'POST',
         body: {
-"channel":  "default",
-"chaincode":  "obcs-marbles",
-"method":  "initMarble",
-"args":  ["marblename","red", 123456,"size"]
-        }
-        },
+                "channel":  "default",
+                "chaincode":  "obcs-marbles",
+                "method":  "initMarble",
+                "args":  ["marblename","red", 123456,"size"]
+              },
         headers: {
           Authorization: "Basic YXBpLnVzZXI6QXNkZmdoMTIzNDU2QA==",
           "Content-Type": "application/json",
          //"cache-control": "no-cache"
-        },
-        json: true,
-        gzip: true,
+                }},
+
         (error, response, body) => {
           console.log('response: ', response.statusCode);
           console.log('body: ', body);
